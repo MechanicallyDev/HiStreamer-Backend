eb0eb624c2cd3dd1b4fd6ed8151fece1324c6cd9
MechanicallyDev <contato@mechanically.dev>
2020-12-25 02:43:36 -0300

    added api prefix for proxy

--- src/entities/posts/routes.js
+++ src/entities/posts/routes.js
@@ -1,10 +1,9 @@
 const Controller = require('./controller');
 const { authMiddlewares } = require('../users/composer');
-const cors = require('cors');
 
 module.exports = (app) => {
-  app.get('/posts', Controller.index);
-  app.get('/post/:slug', Controller.read);
-  app.post('/post', authMiddlewares.bearer, Controller.create);
-  app.delete('/post/:id', authMiddlewares.bearer, Controller.delete);
+  app.get('api/posts', Controller.index);
+  app.get('api/post/:slug', Controller.read);
+  app.post('api/post', authMiddlewares.bearer, Controller.create);
+  app.delete('api/post/:id', authMiddlewares.bearer, Controller.delete);
 };
--- src/entities/users/routes.js
+++ src/entities/users/routes.js
@@ -2,13 +2,13 @@ const Controller = require('./controller');
 const authMiddlewares = require('./auth/middlewares');
 
 module.exports = (app) => {
-  app.post('/user', Controller.create);
-  app.get('/user/verify_email/:token', Controller.verifyEmail);
-  app.post('/user/login', authMiddlewares.local, Controller.login);
+  app.post('api/user', Controller.create);
+  app.get('api/user/verify_email/:token', Controller.verifyEmail);
+  app.post('api/user/login', authMiddlewares.local, Controller.login);
   app.post(
-    '/user/logout',
+    'api/user/logout',
     [authMiddlewares.refresh, authMiddlewares.bearer],
     Controller.logout
   );
-  app.post('/user/refresh_token', authMiddlewares.refresh, Controller.login);
+  app.post('api/user/refresh_token', authMiddlewares.refresh, Controller.login);
 };
