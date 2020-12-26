4f1447f75dc15d30048018e73fe3c407afbbf73f
MechanicallyDev <contato@mechanically.dev>
2020-12-26 17:23:04 -0300

    route for debug

--- src/entities/posts/routes.js
+++ src/entities/posts/routes.js
@@ -2,6 +2,7 @@ const Controller = require('./controller');
 const { authMiddlewares } = require('../users/composer');
 
 module.exports = (app) => {
+  app.get('/api/debug', console.log(process.env.NODE_ENV));
   app.get('/api/posts', Controller.index);
   app.get('/api/post/:slug', Controller.read);
   app.post('/api/post', authMiddlewares.bearer, Controller.create);
