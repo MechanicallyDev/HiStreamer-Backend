7e2b5d3810588d59d0b59abe33e8e2f896a67b45
MechanicallyDev <contato@mechanically.dev>
2020-12-26 15:24:36 -0300

    fix for env config

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,5 @@
-const knex = require('knex');
 require('dotenv').config({ path: '../../' });
 
-const configuration = require('../../knexfile');
-
-const environment = process.env.NODE_ENV || 'development';
-
-const connection = knex(configuration[environment]);
-
-module.exports = connection;
+const environment = process.env.ENVIRONMENT || 'development';
+const config = require('../knexfile.js')[environment];
+module.exports = require('knex')(config);
