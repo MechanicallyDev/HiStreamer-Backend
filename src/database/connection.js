c1eb082b4e2eb8f57617ee69bc7a31a9f366595a
MechanicallyDev <contato@mechanically.dev>
2020-12-26 15:41:48 -0300

    added host on knex (debug)

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: '../../' });
 const config = {
   client: process.env.DATABASE_TYPE,
   connection: {
+    host: '127.0.0.1',
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
