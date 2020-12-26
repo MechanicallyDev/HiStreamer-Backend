964d3f35e676598a4697eeefeeaa711bc421ed5c
MechanicallyDev <contato@mechanically.dev>
2020-12-26 19:46:04 -0300

    remove .env  for PM2 to handle environment var

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,3 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: '../../.env' });
-
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../../knexfile.js')[environment];
 module.exports = require('knex')(config);
