b77ce36bbfb5a19927cef5651dc932ae4e70f826
MechanicallyDev <contato@mechanically.dev>
2020-12-26 14:32:41 -0300

    fix for env on knex file

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,8 @@ const knex = require('knex');
 
 const configuration = require('../../knexfile');
 
-if (process.env.NODE_ENV === 'development')
-  const connection = knex(configuration.development);
-else const connection = knex(configuration.production);
+const environment = process.env.NODE_ENV || 'development';
+
+const connection = knex(configuration[environment]);
+
 module.exports = connection;
